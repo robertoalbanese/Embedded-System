041957551a03d136212d025eeb7ab010223d484f
Robbai <ralbanese18@gmail.com>
2020-10-08 12:06:13 +0200

    Timer 1 ex

--- Week2/Timer1.X/main.c
+++ Week2/Timer1.X/main.c
@@ -34,6 +34,68 @@
 
 #include <xc.h>
 
-int main(void) {
-    return 0;
+#define TIMER1 1
+#define TIMER2 2
+void tmr_setup_period(int timer, int ms);
+void tmr_wait_period(int timer);
+
+int main() {
+    // initialization code
+    TRISBbits.TRISB0 = 0;
+    
+    tmr_setup_period(TIMER1, 1);
+    while (1) {
+        // code to blink LED
+        tmr_wait_period(TIMER1);
+        LATBbits.LATB0 = !LATBbits.LATB0;
+    }
+}
+
+void tmr_setup_period(int timer, int ms) {
+    if (timer == TIMER1) {
+        TMR1 = 0; //reset the timer counter
+        // Fcy = 1843200 (number of clocks in one second)
+        // in 1 second there would be 921600 clocks steps
+        // this is too high to be put in a 16 bit register (max 65535)
+        // If we set a prescaler of 1:64 we have 1843200/16 = 28800 clock steps, OK!
+        float rate_in_sec = (float) ms / 1000000;
+        PR1 = 28800 * rate_in_sec;
+
+        T1CONbits.TCKPS = 2; //prescaler 1:64
+        T1CONbits.TCS = 0; //use internal clock
+        T1CONbits.TON = 1; //starts the timer
+    } else if (timer == TIMER2) {
+        TMR2 = 0; //reset the timer counter
+        // Fcy = 1843200 (number of clocks in one second)
+        // in 1 second there would be 921600 clocks steps
+        // this is too high to be put in a 16 bit register (max 65535)
+        // If we set a prescaler of 1:64 we have 1843200/16 = 28800 clock steps, OK!
+        float rate_in_sec = (float) ms / 1000;
+        PR2 = 28800; // * rate_in_sec;
+
+        T2CONbits.TCKPS = 2; //prescaler 1:64
+        T2CONbits.TCS = 0; //use internal clock
+        T2CONbits.TON = 1; //starts the timer
+    }
+}
+
+void tmr_wait_period(int timer) {
+    switch (timer) {
+        case TIMER1:
+        {
+            while (IFS0bits.T1IF == 0) {
+            }
+            IFS0bits.T1IF = 0;
+            TMR1 = 0;
+            break;
+        }
+        case TIMER2:
+        {
+             while (IFS0bits.T2IF == 0) {
+            }
+            IFS0bits.T2IF = 0;
+            TMR2 = 0;
+            break;
+        }
+    }
 }
